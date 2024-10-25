46bf45793e587810f0cc58e91c3e68a846c3a349
Michael Vairo <i202335261@cibertec.edu.pe>
2024-10-25 17:30:59 -0500

    Initial commit

---
+++ src/main/java/pe/cibertec/pgmatricula/PgmatriculaApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.pgmatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmatriculaApplication.class, args);
+	}
+
+}
