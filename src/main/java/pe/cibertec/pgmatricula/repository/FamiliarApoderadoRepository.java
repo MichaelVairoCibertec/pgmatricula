94e613342dad3c7008d02f488441f2867556d788
Michael Vairo <i202335261@cibertec.edu.pe>
2024-10-25 23:26:05 -0500

    Se creó el repositorio de FamiliarApoderado

---
+++ src/main/java/pe/cibertec/pgmatricula/repository/FamiliarApoderadoRepository.java
@@ -0,0 +1,7 @@
+package pe.cibertec.matricula.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.cibertec.matricula.models.FamiliarApoderado;
+
+public interface FamiliarApoderadoRepository extends JpaRepository<FamiliarApoderado, Long> {
+}
