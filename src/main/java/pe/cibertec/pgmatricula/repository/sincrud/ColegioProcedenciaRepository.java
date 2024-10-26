51278eb72b356ae3bfb0349a20c261b2f435d16a
Michael Vairo <i202335261@cibertec.edu.pe>
2024-10-25 23:26:49 -0500

    Se creó el repositorio de ColegioProcedencia

---
+++ src/main/java/pe/cibertec/pgmatricula/repository/sincrud/ColegioProcedenciaRepository.java
@@ -0,0 +1,7 @@
+package pe.cibertec.matricula.repository.sincrud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.cibertec.matricula.models.sincrud.ColegioProcedencia;
+
+public interface ColegioProcedenciaRepository extends JpaRepository<ColegioProcedencia, Long> {
+}
